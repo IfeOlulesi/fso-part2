206f64d7b6e063f49b562f06cabc19d66361ff8b
Ife Olulesi <53560106+IfeOlulesi@users.noreply.github.com>
2021-06-09 16:45:44 +0100

    2b Forms: Filtering Displayed elements

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,16 +1,54 @@
-import './App.css';
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+  
+  const toggleShowAll = (event) => {
+    event.preventDefault()
+    setShowAll(!showAll)
+  }
+
+  const notesToShow = showAll 
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value = {newNote} onChange={handleNoteChange}/>
+        <button type="submit">Save Note</button>
+        <button onClick={toggleShowAll}>
+          Show {showAll ? "important" : "all"}
+        </button>
+      </form>
     </div>
   )
 }
