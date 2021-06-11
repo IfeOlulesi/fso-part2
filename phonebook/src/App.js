e0e5da69f1c0f00084c1875bc578afc32420f14d
Ife Olulesi <53560106+IfeOlulesi@users.noreply.github.com>
2021-06-11 11:18:25 +0100

    2.11: The Phonebook Step6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react" 
+import React, { useState, useEffect } from "react" 
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import Form from './components/Form'
@@ -8,12 +9,17 @@ const App = () => {
   const [newName, setNewName] = useState("") 
   const [newNumber, setNewNumber] = useState("")
   const [filterParam, setFilterParam] = useState("")
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }  
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log("Effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault() 
