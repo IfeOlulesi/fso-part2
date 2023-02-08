7df8b848340f319a1f546531ad0d6955f84cc31b
IfeOlulesi <ifeolulesi@gmail.com>
2023-02-08 08:49:01 +0100

    Refactored baseurl

--- practice/src/App.js
+++ practice/src/App.js
@@ -77,7 +77,6 @@ const App = () => {
   };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
--- practice/src/services/notes.js
+++ practice/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // const baseUrl = `http://localhost:3005/notes`
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
